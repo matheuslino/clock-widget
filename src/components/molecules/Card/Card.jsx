ddfadab8e6e9d15d6ff5863b165804630a8b2fd3
Matheus A Lino <matheus_araujolino@hotmail.com>
2022-12-01 00:32:00 -0500

    [fix] updated Card component

--- src/components/molecules/Card/Card.jsx
+++ src/components/molecules/Card/Card.jsx
@@ -1,9 +1,10 @@
 import Digit from '../../atoms/Digit/Digit'
 import styles from './style.module.css'
 
-const Card = ({ text, flagged }) => {
+const Card = ({ text, time, period }) => {
   const render = (
     <div className={styles.Card}>
+      <div className={styles.flag}>{period}</div>
       <Digit>{text?.slice(0, 1)}</Digit>
       <Digit>{text?.slice(1, 2)}</Digit>
     </div>
--- src/components/organisms/Clock/Clock.jsx
+++ src/components/organisms/Clock/Clock.jsx
@@ -1,10 +1,18 @@
+import { useEffect, useRef } from 'react'
 import Card from '../../molecules/Card/Card'
 import styles from './style.module.css'
 
 const Clock = () => {
+  const timer = useRef(null)
+
+  //   useEffect(() => {
+  //     const time = setTimeout(() => new Date(), 1000)
+  //     return () => clearTimeout(time)
+  //   }, [])
+
   const render = (
     <div className={styles.clock}>
-      <Card text='12' flagged />
+      <Card text='12' time={new Date()} period='AM' />
       <Card text='5' />
       <Card text='27' />
     </div>
