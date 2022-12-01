7faf2adedac6886fdd3e08bd4c7f92ea53a47721
Matheus A Lino <matheus_araujolino@hotmail.com>
2022-12-01 02:33:18 -0500

    [add] component Clock and functionality

--- src/components/molecules/Card/Card.jsx
+++ src/components/molecules/Card/Card.jsx
@@ -1,12 +1,17 @@
 import Digit from '../../atoms/Digit/Digit'
 import styles from './style.module.css'
 
-const Card = ({ text, time, period }) => {
+const Card = ({ text, period }) => {
   const render = (
     <div className={styles.Card}>
-      <div className={styles.period}>{period}</div>
-      <Digit>{text?.slice(0, 1)}</Digit>
-      <Digit>{text?.slice(1, 2)}</Digit>
+      {period ? (
+        <>
+          <div className={styles.period}>{text < 12 ? 'AM' : 'PM'}</div>
+          <Digit>{text?.slice(-2)}</Digit>
+        </>
+      ) : (
+        <Digit>{('0' + text)?.slice(-2)}</Digit>
+      )}
     </div>
   )
 
--- src/components/organisms/Clock/Clock.jsx
+++ src/components/organisms/Clock/Clock.jsx
@@ -1,22 +1,29 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import Card from '../../molecules/Card/Card'
 import styles from './style.module.css'
 
 const Clock = () => {
-  const timer = useRef(null)
+  const [timer, setTimer] = useState()
 
-  //   useEffect(() => {
-  //     const time = setTimeout(() => new Date(), 1000)
-  //     return () => clearTimeout(time)
-  //   }, [])
+  useEffect(() => {
+    const localTimer = setInterval(() => {
+      let now = new Date()
+      setTimer({
+        hours: now.getHours() + '',
+        minutes: now.getMinutes() + '',
+        seconds: now.getSeconds() + '',
+      })
+    }, 1000)
+    return () => clearInterval(localTimer)
+  }, [])
 
-  const render = (
+  const render = timer && (
     <>
-      <hr class={styles.divider} />
+      <hr className={styles.divider} />
       <div className={styles.clock}>
-        <Card text='11' time={new Date()} period='AM' />
-        <Card text='5' />
-        <Card text='27' />
+        <Card text={timer.hours} period />
+        <Card text={timer.minutes} />
+        <Card text={timer?.seconds} />
       </div>
     </>
   )
