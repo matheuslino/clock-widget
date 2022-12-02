c6ce0edacbf4931835669663951cf6448513aa61
Matheus A Lino <matheus_araujolino@hotmail.com>
2022-12-02 02:38:55 -0500

    [add] fullscreen togle

--- src/components/molecules/Fullscreen/Fullscreen.jsx
+++ src/components/molecules/Fullscreen/Fullscreen.jsx
@@ -1,12 +1,25 @@
+import { useState, useEffect } from 'react'
 import Maximize from '../../atoms/icons/Maximize'
 import Minimize from '../../atoms/icons/Minimize'
 import styles from './style.module.css'
 
 const Fullscreen = () => {
+  const [isFullscreen, setIsFullscreen] = useState(false)
+
+  const toggleIcon = () => setIsFullscreen((prev) => !prev)
+
+  useEffect(() => {
+    if (isFullscreen) document.body.requestFullscreen()
+    else if (document.fullscreenElement) document.exitFullscreen()
+  }, [isFullscreen])
+
   const render = (
-    <div className={styles.container}>
-      <Maximize color='#fefefe' />
-      <Minimize color='#fefefe' />
+    <div className={styles.container} onClick={toggleIcon}>
+      {isFullscreen ? (
+        <Minimize color='#fefefe' />
+      ) : (
+        <Maximize color='#fefefe' />
+      )}
     </div>
   )
 
